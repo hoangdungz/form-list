c71eaff4c8cc529c98815db36e96e8687e4b6cb1
Dung Hoang Anh <dung.hoang@orientsoftware.com>
2019-10-18 11:27:31 +0700

    update

--- src/components/toDoList/Test.js
+++ src/components/toDoList/Test.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event)  {
+        this.setState({value: event.target.value});
+    }
+    handleSubmit(event)  {
+        alert('name: ' + this.state.value);
+        event.preventDefault();
+    }
     
     render() {
         return (
             <div className="form-group">
-            <form >
-            <input type="text" className="form-control" name id aria-describedby="helpId" placeholder />
-            <button type="reset" className="btn btn-info">ADD ME</button>
-            </form>
+                <form >
+                <input onChange={this.handleChange} type="text" className="form-control" name id aria-describedby="helpId" placeholder />
+                <button onClick = {this.handleSubmit} type="reset" className="btn btn-info">ADD ME</button>
+                </form>
             </div>
         );
     }
