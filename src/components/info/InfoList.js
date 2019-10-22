cf8007bc9938bcfc3f7e19ec4b837141d7693f4e
Dung Hoang Anh <dung.hoang@orientsoftware.com>
2019-10-22 16:57:33 +0700

    update

--- src/components/info/InfoList.js
+++ src/components/info/InfoList.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import './InfoList.css';
 
-
 class FormName extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {            
-            list: [],   
-            fnEdit: '',
-            lsEdit: '', 
-            isEdit: false,
-            indexItem: null,
-        };
-        this.initState = {
-            lastName: '',
-            firstName: ''
-        };
-    }
+    state = {
+        list: [],
+        isEdit: false,
+        indexItem: null,
+    };
+    initState = {
+        lastName: '',
+        firstName: ''
+    };
+    
 
     handleChange = (evt) => {
         this.setState({ [evt.target.name]: evt.target.value });
@@ -28,28 +21,45 @@ class FormName extends Component {
         evt.preventDefault();
     }
 
-
     //TODO add user name
     handleClick = () => {
         //console.log(this.state);
-
+        if (this.state.isEdit === false) {
         const newList = [...this.state.list];
         newList.push({ firstName: this.state.firstName, lastName: this.state.lastName })
-        this.setState({ list: newList } );
+        this.setState({ list: newList });
+
         // reset input
-        this.setState({...this.initState});
-        //console.log(list);
+        this.setState({ ...this.initState });
+        } 
+        
+        if(this.state.isEdit === true) {
+            let arr = [...this.state.list];
+            arr.map((value, key) => {
+                if (key === this.state.indexItem) {
+                    
+                    value.firstName = this.state.firstName;
+                    value.lastName = this.state.lastName;
+                 
+                }
+                console.log(value);
+                return value;
+            })
+            this.setState({
+                isEdit: false,
+                list: arr
+            });
+        }
+        
+        console.log(this.state.isEdit);
+       
     }
 
     handleEdit = (item, index) => {
-        // const array = [...this.state.list];
-        // var index = array.indexOf(item);
-        // array[index].isEdit = !array[index].isEdit;
-
         this.setState({
             firstName: item.firstName,
             lastName: item.lastName,
-            isEdit: true, 
+            isEdit: true,
             indexItem: index,
         })
     }
@@ -58,9 +68,9 @@ class FormName extends Component {
         const array = [...this.state.list];
         var index = array.indexOf(item);
 
-        if(index !== -1) {
+        if (index !== -1) {
             array.splice(index, 1);
-            this.setState({list: array});
+            this.setState({ list: array });
         }
     }
 
@@ -71,9 +81,6 @@ class FormName extends Component {
             isEdit: false,
             indexItem: null,
         });
-
-
-
     }
 
     render() {
@@ -81,28 +88,33 @@ class FormName extends Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <input 
+                        <input
+                            className="form-control"
                             type="text"
                             value={this.state.firstName}
-                            className="form-control" name="firstName" onChange={this.handleChange} aria-describedby="helpId" placeholder="First name: " />
+                            name="firstName" onChange={this.handleChange} aria-describedby="helpId" placeholder="First name: " 
+                        />
                     </div>
                     <div className="form-group">
-                        <input 
+                        <input
+                            className="form-control"
                             type="text"
                             value={this.state.lastName}
-                            className="form-control" name="lastName" onChange={this.handleChange} aria-describedby="helpId" placeholder="Last name: " />
+                            name="lastName" onChange={this.handleChange} aria-describedby="helpId" placeholder="Last name: " 
+                        />
                     </div>
-
                     <button 
-                        style={{ marginBottom: 10, marginRight: 10 }} 
-                        type="reset" 
+                        className="btn btn-info"
+                        style={{ marginBottom: 10, marginRight: 10 }}
+                        type="reset"                       
+                        onClick={this.handleClick}>{this.state.isEdit ? 'UPDATE' : 'ADD'}
+                    </button>
+                    <button 
                         className="btn btn-info" 
-                        onClick={this.handleClick}>{this.state.isEdit ? 'EDIT' : 'ADD'}
+                        style={{ marginBottom: 10 }} 
+                        type="reset"           
+                        onClick={this.handleCancelEdit}>CANCEL
                     </button>
-                    <button style={{ marginBottom: 10 }} type="reset" className="btn btn-info" onClick={this.handleCancelEdit}>
-                        CANCEL
-                    </button>
-
                 </form>
                 <table className="table">
                     <thead>
@@ -113,20 +125,27 @@ class FormName extends Component {
                             <th>Button</th>
                         </tr>
                     </thead>
-                    <tbody>            
-                            {this.state.list.map((item, index) => {
-                                return (
-                                    <tr style={{backgroundColor: this.state.indexItem === index ? '#e6f7ff' : '#ffffff' }} key={index}>
-                                        <td>{index + 1}</td>
-                                        <td>{item.firstName}</td>
-                                        <td>{item.lastName}</td>
-                                        <td>
-                                            <button className="btn btn-info" style={{marginRight: 10}} onClick={() => this.handleEdit(item, index)}>EDIT</button>
-                                            <button className="btn btn-info" onClick={() => this.handleDelete(item)}>DELETE</button>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
+                    <tbody>
+                        {this.state.list.map((item, index) => {
+                            return (
+                                <tr style={{ backgroundColor: this.state.indexItem === index ? '#e6f7ff' : '#ffffff' }} key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>
+                                        <button 
+                                            className="btn btn-info" 
+                                            style={{ marginRight: 10 }} 
+                                            onClick={() => this.handleEdit(item, index)}>EDIT
+                                        </button>
+                                        <button 
+                                            className="btn btn-info" 
+                                            onClick={() => this.handleDelete(item)}>DELETE
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
                 </table>
             </div>
